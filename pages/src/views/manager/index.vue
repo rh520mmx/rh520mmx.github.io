7987bb919f6033e2385e416bbf1800dc56417fa9
huangmeng <huangmeng@mingyuanyun.com>
2019-12-24 11:39:42 +0800

    ces

--- pages/src/views/manager/index.vue
+++ pages/src/views/manager/index.vue
@@ -1,61 +1,43 @@
 <template>
   <div class="app-container">
-    <el-button @click="onGoMdEvent">测试跳转</el-button>
-    <el-button @click="onGoMdEvent">测试跳转</el-button>
-    <el-button @click="onGoMdEvent">测试跳转</el-button>
-    <el-button @click="onGoMdEvent">测试跳转</el-button>
-    <el-button @click="onGoMdEvent">测试跳转</el-button>
+    <el-button @click="onGoMdEvent">测试跳转gitbook主页</el-button>
+    <el-button @click="onGoMdEvent1">测试跳转指定页面</el-button>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name: 'Dashboard',
-  components: {  },
-  data() {
-    return {
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'UserName',
-    ]),
-  },
-  created() {
+  export default {
+    name: 'Dashboard',
+    components: {  },
+    data() {
+      return {
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'UserName',
+      ]),
+    },
+    created() {
 
-  },
-  methods: {
-    onGoMdEvent(){
-      // this.$router.push(`${process.env.VUE_APP_BASE_API}/#/docs`)
-      this.$router.push(`docs/doc/test1.html`)
-      // window.location.href = `/#/docs/doc/test1.html`
-    }
-  },
-}
+    },
+    methods: {
+      onGoMdEvent(){
+        // this.$router.push(`${process.env.VUE_APP_BASE_API}/#/docs`)
+        const domain = document.domain
+        this.$router.push(`${domain}/docs`)
+      },
+      onGoMdEvent1(){
+        const domain = document.domain
+        this.$router.push(`${domain}/docs/doc/test1.html`)
+        // window.location.href = `/#/docs/doc/test1.html`
+      },
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
-  @import "~@/styles/variables.scss";
-  .search{
-    width: 270px;
-    margin-right: $boxMargin;
-  }
-  .el-icon-search{
-    line-height: 32px !important;
-  }
-  .search-section{
-    margin-bottom: $margin;
-    .search-label{
-      display: inline-block;
-      float: left;
-      font-size: $fontSize;
-      color: $fontColorStrong;
-      line-height: 24px;
-    }
-  }
-  .more-search{
-    margin-bottom: $margin;
-  }
+
 </style>

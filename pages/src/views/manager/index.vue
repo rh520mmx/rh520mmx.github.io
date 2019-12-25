e4a6c88ed614704b5f9ab53845ff39ae5bf96946
huangmeng <huangmeng@mingyuanyun.com>
2019-12-25 14:37:19 +0800

    修改链接

--- pages/src/views/manager/index.vue
+++ pages/src/views/manager/index.vue
@@ -27,7 +27,7 @@
         const domain = document.domain
         // this.$router.push(`${domain}/docs/doc/test1.html`)
         console.log(domain);
-        window.location.href = `https://${domain}/docs/doc/test1.html`
+        window.location.href = `https://${domain}/docs/_book/doc/test1.html`
       },
     },
   }
